026a7c32a0a810269c7b66b9e92dc5dabbff4fb3
Thanya R <159054147+thanyarad@users.noreply.github.com>
2024-07-18 15:48:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__) #creating the Flask class object   
  
 @app.route('/') #decorator drfines the   
-def home():  
-    return "hello, this is our first flask website";  
+def average():  
+    return "Average_Calculation";  
   
 if __name__ =='__main__':  
     app.run(debug = True)  
