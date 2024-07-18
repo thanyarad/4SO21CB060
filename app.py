98f3928b44ea1daa2013ec008523a5593353a0ae
Thanya R <159054147+thanyarad@users.noreply.github.com>
2024-07-18 15:44:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask  
+  
+app = Flask(__name__) #creating the Flask class object   
+ 
+@app.route('/') #decorator drfines the   
+def home():  
+    return "hello, this is our first flask website";  
+  
+if __name__ =='__main__':  
+    app.run(debug = True)  
